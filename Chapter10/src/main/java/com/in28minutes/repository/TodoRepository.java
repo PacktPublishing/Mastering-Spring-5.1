58f52c480f319d8c3e673ae24fcb98d50f041880
roycejpackt <46219239+roycejpackt@users.noreply.github.com>
2019-05-21 14:18:18 +0530

    Add files via upload

---
+++ Chapter10/src/main/java/com/in28minutes/repository/TodoRepository.java
@@ -0,0 +1,22 @@
+package com.in28minutes.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.in28minutes.model.Todo;
+
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+	List<Todo> findByTitleAndDescription(String title, String description);
+
+	List<Todo> findDistinctTodoByTitleOrDescription(String title,
+			String description);
+
+	List<Todo> findByTitleIgnoreCase(String title);
+
+	List<Todo> findByTitleOrderByIdDesc(String title);
+	
+	List<Todo> findByIsDoneTrue();
+
+}
