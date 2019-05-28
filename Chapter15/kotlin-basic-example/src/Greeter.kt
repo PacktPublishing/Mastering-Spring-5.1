1e16164b4fd99be0496954ae144e90082015dd7e
Royce <roycej@packt.com>
2019-05-28 12:36:30 +0530

    Removing

--- Chapter15/kotlin-basic-example/src/Greeter.kt
+++
@@ -1,9 +0,0 @@
-class Greeter(val name : String) { 
-  fun greet() { 
-    println("Hello, ${name}"); 
-  } 
-} 
- 
-fun main(args : Array<String>) { 
-  Greeter("John Doe").greet() 
-}
--- Chapter15/kotlin-basic-example/src/HelloWorld.kt
+++
@@ -1,7 +0,0 @@
-package com.mastering.spring.kotlin.basics
-
-//It is not required to match directories and packages: source files can be placed arbitrarily in the file system
-
-fun main(args: Array<String>) {
-	println("Hello, world!")
-}
